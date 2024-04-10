2503c97192e7b20cffd127000671f280c301fd2a
bobbyhadz <opmftw@gmail.com>
2024-04-10 09:13:40 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Get the first character of each string in a List in Python
+
+my_list = ['bobby', 'hadz', 'com']
+
+
+new_list = [item[0] for item in my_list]
+print(new_list)  # 👉️ ['b', 'h', 'c']
